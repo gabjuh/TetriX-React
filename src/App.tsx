b171dd794efa79416d0e4183b698d63913ac2808
gabjuh <jjjjgabesz@gmail.com>
2023-10-19 11:06:49 +0200

    create basic react ts tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="mt-10 text-red-400">ads</p>
+    </>
+  )
+}
+
+export default App
