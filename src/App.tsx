343c1a8e8bbd63b040f60c21fdbb16bebe3dd17f
gabjuh <jjjjgabesz@gmail.com>
2023-10-19 11:55:21 +0200

    cleanup, add Canvas component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import Canvas from './Components/Canvas'
 
 function App() {
 
   return (
     <>
-      <p className="mt-10 text-red-400">ads</p>
+      <Canvas />
     </>
   )
 }
